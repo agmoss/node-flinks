c359b1dad578d709f61c3636a12249484cf3df03
Dennis Barthel <dennis.barthel@neofinancial.com>
2022-11-04 12:10:12 -0600

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import FlinksClient from './client';
 
 export {
-  FlinksGetAccountsDetailAsyncResponse, FlinksGetAccountsDetailResponse, GetAccountsDetailAsyncOptions, GetAccountsDetailAsyncResponse, GetAccountsDetailOptions, GetAccountsDetailResponse
+  FlinksGetAccountsDetailAsyncResponse,
+  FlinksGetAccountsDetailResponse,
+  GetAccountsDetailAsyncOptions,
+  GetAccountsDetailAsyncResponse,
+  GetAccountsDetailOptions,
+  GetAccountsDetailResponse,
 } from './commands/accounts-detail';
 export {
-  FlinksGetAccountsSummaryAsyncResponse, FlinksGetAccountsSummaryResponse, GetAccountsSummaryAsyncOptions, GetAccountsSummaryAsyncResponse, GetAccountsSummaryOptions, GetAccountsSummaryResponse
+  FlinksGetAccountsSummaryAsyncResponse,
+  FlinksGetAccountsSummaryResponse,
+  GetAccountsSummaryAsyncOptions,
+  GetAccountsSummaryAsyncResponse,
+  GetAccountsSummaryOptions,
+  GetAccountsSummaryResponse,
 } from './commands/accounts-summary';
 export { AuthorizeOptions, AuthorizeResponse, FlinksAuthorizeResponse } from './commands/authorize';
 export {
-  FlinksGetStatementsAsyncResponse, FlinksGetStatementsResponse, GetStatementsAsyncOptions, GetStatementsAsyncResponse, GetStatementsOptions, GetStatementsResponse
+  FlinksGetStatementsAsyncResponse,
+  FlinksGetStatementsResponse,
+  GetStatementsAsyncOptions,
+  GetStatementsAsyncResponse,
+  GetStatementsOptions,
+  GetStatementsResponse,
 } from './commands/statements';
 export * from './lib/authenticity';
 export { FlinksErrorResponse } from './lib/flinks-error';
